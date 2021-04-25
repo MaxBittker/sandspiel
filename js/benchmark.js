e4eaf090f929c19838d58d7c5e3d1a99fd76bed1
max bittker <maxbittker@gmail.com>
2021-04-25 00:00:24 -0400

    benchmark

---
+++ js/benchmark.js
@@ -0,0 +1,54 @@
+import { Universe, Species } from "../crate/pkg";
+
+// function clear() {
+//   u.paint(150, 150, 600, 0);
+// }
+// let
+function setup() {
+  let n = 300;
+  let h = n / 2;
+  let d = n - 6;
+  let universe = Universe.new(n, n);
+  universe.paint(h, h, d + 2, Species.Plant);
+  universe.paint(h - 30, d - 3, 20, Species.Fire);
+  universe.paint(h + 30, d - 3, 20, Species.Fire);
+  universe.paint(h, h, n / 3, Species.Air);
+  universe.paint(h, h, n / 3, Species.Fire);
+  return universe;
+}
+function functionUnderTest(u) {
+  u.tick();
+}
+function trial(m) {
+  let input = setup();
+  const t0 = performance.now();
+  for (let i = 0; i < m; i++) {
+    functionUnderTest(input);
+  }
+
+  const t1 = performance.now();
+  let delta = t1 - t0;
+  return delta;
+}
+function runTest(n, m) {
+  let min = Infinity;
+  let max = 0;
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    let delta = trial(m);
+    min = Math.min(delta, min);
+    max = Math.max(delta, max);
+    sum += delta;
+    console.log(`${i}/${n}`);
+  }
+  let avg = sum / n;
+  console.log(`Ran ${n} trials
+Range per ${m}: 
+${min.toPrecision(3)}ms - ${max.toPrecision(3)}ms
+avg: ${(avg / m).toPrecision(3)}ms
+  `);
+}
+function runBenchmark() {
+  runTest(20, 20);
+}
+export { runBenchmark };
