79a146613c7cf6e335c6d46f899d0f677eaa7cda
max bittker <maxbittker@gmail.com>
2022-06-10 16:28:36 -0400

    promo vid

--- js/components/promotab.js
+++ js/components/promotab.js
@@ -1,20 +1,39 @@
 import React, { useState } from "react";
 // import "./styles.css";
+import YouTube from "react-youtube";
 
 export default function PullTab() {
   let [open, setOpen] = useState("");
   return (
     <div id="PullTabContent" className={open}>
-      <img
+      {/* <img
         id="logo"
         src="assets/SSStudio ColorLined Big.svg"
         alt="SANDSPIEL STUDIO"
-      />
+      /> */}
       <p>
-        {" "}
+        <YouTube
+          videoId="ecCVor7mJ6o"
+          opts={{
+            height: "255",
+            width: "455",
+            playerVars: {
+              // https://developers.google.com/youtube/player_parameters
+            },
+          }}
+        />{" "}
         Explore infinite new elements and create your own worlds in Sandspiel
         Studio™, releasing this summer!
       </p>
+      <a
+        className="CTA"
+        href="https://sandspiel.gumroad.com/l/studio-early-access"
+      >
+        {" "}
+        Sign up for Early Access!
+      </a>
+      <br></br>
+      <i>OR</i>
       {/* <br></br>
       <a href="example.com" className="discord">
         <img className="arrow-icon" src="assets/arrow.png"></img> Early access
@@ -23,7 +42,6 @@ export default function PullTab() {
       </a> */}
       {/* <img className="arrow-icon reversed" src="assets/arrow.png"></img> */}
       {/* <br></br> */}
-
       <br></br>
       <span>Sign up for updates:</span>
       <iframe
@@ -32,7 +50,6 @@ export default function PullTab() {
         style={{ overflow: "hidden", border: "none", marginTop: -30 }}
         src="https://cdn.forms-content.sg-form.com/a073cf1c-d71c-11ec-9afb-3ada83737b45"
       />
-
       <p>
         See also:{" "}
         <a href="https://orb.farm" target="_blank">
