85d7876cb5c349e41e5ed56459d5b33d1bc5d672
MaxBittker <MaxBittker@users.noreply.github.com>
2025-10-20 19:53:47 +0000

    deploy: 72cee46b2ab13d2f64f636634761e94c17165f65

---
+++ preview/claude/test-baseline-code-011CUJzrkaNgT6Qeqxsp811E/precache-manifest.8e183b4861156dfd5d41576128b190d2.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51ed00592e588e5c973",
+    "url": "/1.e0b9b79d263c70ef8b40.js"
+  },
+  {
+    "revision": "7315e5c45c0158135d40",
+    "url": "/2.5007c9eb092c30b2bd3e.js"
+  },
+  {
+    "revision": "7315e5c45c0158135d40",
+    "url": "/31716942d7474bb5325f.module.wasm"
+  },
+  {
+    "revision": "e6bedc717c42d8066a53aecf3e1d0a7c",
+    "url": "/assets/ChiKareGo2.ttf"
+  },
+  {
+    "revision": "3fc5383ee0b7e708a6ba43d610541eb0",
+    "url": "/assets/PPMondwest-Regular.otf"
+  },
+  {
+    "revision": "f67a0ca88b3c94ce1ee9c3ab0fac8385",
+    "url": "/assets/PPNeueBit-Bold.otf"
+  },
+  {
+    "revision": "f95644c059701367eab42301ab809ad7",
+    "url": "/assets/PPNeueBit-Regular.otf"
+  },
+  {
+    "revision": "0c2508db2e5131814c550839016bfa1d",
+    "url": "/assets/SSStudio ColorLined Big.svg"
+  },
+  {
+    "revision": "ae9662e2c30c9563a92d508011bd83c2",
+    "url": "/assets/ads.txt"
+  },
+  {
+    "revision": "fec83330d41c4f4c915dae7f6a8220d6",
+    "url": "/assets/android-chrome-192x192.png"
+  },
+  {
+    "revision": "366424b29cdd7085fd8bbabc6a8c3b7e",
+    "url": "/assets/android-chrome-256x256.png"
+  },
+  {
+    "revision": "4b1042b5254bd54801ba1e08f576381e",
+    "url": "/assets/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "f92d754288ab63ed7f4d57d61828f2ff",
+    "url": "/assets/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "d37c95c2a2895151be72f6b70ea124b2",
+    "url": "/assets/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "0c6fce5a532a3d886edd8fc751f6fcc2",
+    "url": "/assets/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "b0f8318addac626ff4ebf4fb2749c112",
+    "url": "/assets/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "d37c95c2a2895151be72f6b70ea124b2",
+    "url": "/assets/apple-touch-icon.png"
+  },
+  {
+    "revision": "499c2a9f7d9810e5447b63498a4e471e",
+    "url": "/assets/arrow.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/assets/browserconfig.xml"
+  },
+  {
+    "revision": "8541fad56563eeb80148e26505bc1c07",
+    "url": "/assets/favicon-16x16.png"
+  },
+  {
+    "revision": "bc003177d3e0fa86787753c4ae8f6f5d",
+    "url": "/assets/favicon-32x32.png"
+  },
+  {
+    "revision": "395cf26201869d58ec7c42b2f31e40f8",
+    "url": "/assets/favicon.ico"
+  },
+  {
+    "revision": "3ed59c98ec041d25145e97a30e3e3a0c",
+    "url": "/assets/html_code.html"
+  },
+  {
+    "revision": "a80bc7944295e138a720a5cccf9f470c",
+    "url": "/assets/icon-128x128.png"
+  },
+  {
+    "revision": "5dac5ef2ee1015e767a9565eae6dcd31",
+    "url": "/assets/icon-144x144.png"
+  },
+  {
+    "revision": "dde90c3841da891ae50d23bc3e7b745a",
+    "url": "/assets/icon-152x152.png"
+  },
+  {
+    "revision": "a2168e20c425427f42d996307b016137",
+    "url": "/assets/icon-192x192.png"
+  },
+  {
+    "revision": "95beb5007f4e3e213b4d71b064d30bd0",
+    "url": "/assets/icon-384x384.png"
+  },
+  {
+    "revision": "e5d218f15e51ca7a2d6dbcbe1ff2b7a9",
+    "url": "/assets/icon-512x512.png"
+  },
+  {
+    "revision": "cb8877cfcc0e3b90c102b04a37bdaae6",
+    "url": "/assets/icon-72x72.png"
+  },
+  {
+    "revision": "e8f0d15a9b2b54322bd0255687f3dc32",
+    "url": "/assets/icon-96x96.png"
+  },
+  {
+    "revision": "2245825a2bc86406252146161025a57b",
+    "url": "/assets/mstile-150x150.png"
+  },
+  {
+    "revision": "ea3b282b5f34761cd3b701d4036d040a",
+    "url": "/assets/price.png"
+  },
+  {
+    "revision": "f743400f99c2693af1edf6931bc761e6",
+    "url": "/assets/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "25fc626cd69629cb4e1cac4c9c7c8997",
+    "url": "/assets/site.webmanifest"
+  },
+  {
+    "revision": "2d9879a6af4757338bbdd1da32a4a465",
+    "url": "/assets/tab.png"
+  },
+  {
+    "revision": "07c7bb32dc81aece7902c8be7e684617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a7bfec76eef61d7c1b",
+    "url": "/main.b91eb86159094458d977.js"
+  },
+  {
+    "revision": "cf96bc11f0a11bbf4b6bda0f88289b6e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "1a82449531ee3f5dbad00dacfee551c3",
+    "url": "/styles.css"
+  }
+]);
---
+++ preview/claude/test-baseline-code-011CUJzrkaNgT6Qeqxsp811E/service-worker.js
@@ -0,0 +1,37 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.8e183b4861156dfd5d41576128b190d2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"));
+
+workbox.routing.registerRoute(/\.html$/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|svg)$/, new workbox.strategies.CacheFirst({ "cacheName":"images", plugins: [new workbox.expiration.Plugin({ maxEntries: 300, purgeOnQuotaError: false })] }), 'GET');
